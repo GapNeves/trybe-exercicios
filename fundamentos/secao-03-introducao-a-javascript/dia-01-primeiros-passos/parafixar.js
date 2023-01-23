328b50345111cceb6876b0ca7d597958eeb04218
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-01-23 18:50:10 -0300

    Realizando exercicio para fixar Operadores Logico, relogio da fome

--- fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/parafixar.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/parafixar.js
@@ -50,3 +50,26 @@
 // }
 
 // console.log(result);
+
+//Atividade para fixar-JavaScript-Primeiros passos-Operadores Lógicos
+
+// const currentHour = 8;
+// let message = null;
+
+// if (currentHour >= 22){
+//     message = 'Não deveríamos comer nada, está na hora de dormir'
+// }
+// else if(currentHour >= 18 && currentHour < 22){
+//     message = 'Rango da noite, vamos jantar :D'
+// }
+// else if(currentHour >=14 && currentHour < 18){
+//     message = 'Vamos fazer um bolo pro café da tarde?'
+// }
+// else if(currentHour >= 11 && currentHour < 14){
+//     message = 'Hora do almoço!!!'
+// }
+// else if(currentHour == 4 || currentHour <= 11){
+//     message = 'Hmmm, cheiro de café recém-passado'
+// }
+
+// console.log(message);
