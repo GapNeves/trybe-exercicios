8b90e4d77e5af142335a70702a42530920143645
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-01-25 19:11:37 -0300

    exercicio faotrial

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritimos/agoraPratica.js
@@ -0,0 +1,11 @@
+//Atividade para fixar-JavaScript-Lógica de Programação-Fatorial
+
+const number = 10;
+let fatorial = 1;
+
+for (let index = number; index > 1; index -=1){
+    fatorial = fatorial * index ;
+}
+
+console.log(fatorial);
+
