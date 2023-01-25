62eccbb6e45c31ef59e20cfafab15eb1927de0cd
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-01-25 10:58:06 -0300

    Iniciando exercicios agora a pratica

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/agoraPratica.js
@@ -0,0 +1,8 @@
+const a = 35;
+const b = 27;
+
+console.log('Adição ' + a + ' + ' + b + ' = ' + ( a + b) );
+console.log('Subitração ' + a + ' - ' + b + ' = ' + ( a - b) );
+console.log('Multiplicação '  + a + ' * ' + b + ' = ' + ( a * b) );
+console.log('Divisão ' + a + ' / ' + b + ' = ' + ( a / b) );
+console.log('Módulo ' + a + ' % ' + b + ' = ' + ( a % b) );
