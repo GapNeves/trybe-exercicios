26daca8ca564320c4ecc90e09297febdf982294d
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-01-25 11:10:30 -0300

    finalizando maior número

--- fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/agoraPratica.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/agoraPratica.js
@@ -6,3 +6,21 @@ console.log('Subitração ' + a + ' - ' + b + ' = ' + ( a - b) );
 console.log('Multiplicação '  + a + ' * ' + b + ' = ' + ( a * b) );
 console.log('Divisão ' + a + ' / ' + b + ' = ' + ( a / b) );
 console.log('Módulo ' + a + ' % ' + b + ' = ' + ( a % b) );
+
+const num1 =15;
+const num2 =12;
+const num3 =27;
+let numMaior = '';
+
+if(num1 > num2 && num1 > num3){
+    numMaior = num1 + ' é o maior entre os números!'
+}
+else if(num2 > num1 && num2 > num3){
+    numMaior = num2 + ' é o maior entre os números!'
+}
+else{
+    numMaior = num3 + ' é o maior entre os números!'
+}
+console.log(numMaior);
+
+
