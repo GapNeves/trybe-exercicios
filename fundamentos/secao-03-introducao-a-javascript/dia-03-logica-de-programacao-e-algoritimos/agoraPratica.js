f578b7153c219f552b073a94f5e004e2155bb7d7
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-01-25 22:25:40 -0300

    finalizando invertendo palavras!

--- fundamentos/secao-03-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritimos/agoraPratica.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-logica-de-programacao-e-algoritimos/agoraPratica.js
@@ -12,4 +12,10 @@ console.log(fatorial);
 //Atividade para fixar-JavaScript-Lógica de Programação-Invertendo palavras
 
 let word = 'tryber';
+let reverseWord = '';
 
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
+}
+
+console.log(reverseWord);
