342424cf47540261eb34b0cc8ac0d3cc762ef0f1
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-01-26 16:03:43 -0300

    segunda parte concluida

--- fundamentos/secao-03-introducao-a-javascript/dia-04-objetos-e-funcoes/paraFixar.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-objetos-e-funcoes/paraFixar.js
@@ -5,4 +5,7 @@ let player = {
     medals : { golden: 2, silver: 3 },
 }
 
-console.log('A jogadora ' + player.nome + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.')
+console.log('A jogadora ' + player.nome + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
