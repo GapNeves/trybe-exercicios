e8e9dad69a438a3da965557a1da644223b33da22
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-01-26 15:55:21 -0300

    priemira parte concluida

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-objetos-e-funcoes/paraFixar.js
@@ -0,0 +1,8 @@
+let player = {
+    nome : 'Marta',
+    lastName : 'Silva',
+    age : 34,
+    medals : { golden: 2, silver: 3 },
+}
+
+console.log('A jogadora ' + player.nome + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.')
