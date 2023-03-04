a32d383a8bd3de4f674d2183eb21b78a4db3e418
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-03-04 12:02:34 -0300

    finalizando Exercício agora a prática - Hofs

---
+++ fundamentos/secao-07-Introducao-a-HOF/dia-01-introducao-a-HOF/exercise.js
@@ -0,0 +1,106 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+//Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+const authorBornIn1947 = () => books.find((book) => book.author.birthYear === 1947).author.name;
+
+//Retorne o nome do livro com menor número de caracteres (menor nome).
+const smallerName = () => {
+  let nameBook;
+  books.forEach((element) => {
+    if (!nameBook || element.name.length < nameBook.length) {
+      nameBook = element.name;
+    }
+  });
+  return nameBook;
+}
+
+//Encontre o primeiro livro cujo nome possua 26 caracteres.
+const getNamedBook = () => {
+  return books.find((book) => book.name.length === 26);
+}
+
+//Faça uma função que retorne true se todas as pessoas autoras tiverem nascido no século XX, ou false, caso contrário.
+const expectedBool = false;
+
+const everyoneWasBornOnSecXX = () => {
+  return books.every((book) => (
+    book.author.birthYear >= 1901 && book.author.birthYear <= 2000
+  ));
+}
+
+//Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
+const expectedResult = true;
+
+const someBookWasReleaseOnThe80s = () => {
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989);
+}
+
+//Faça uma função que retorne true, caso nenhuma pessoa autora tenha nascido no mesmo ano, e false, caso contrário.
+const expectedoutherResult = false;
+
+const authorUnique = () => {
+  return books.every((book) =>
+    !books.some((bookSome) =>
+      (bookSome.author.birthYear === book.author.birthYear)
+      && (bookSome.author.name !== book.author.name)));
+}
