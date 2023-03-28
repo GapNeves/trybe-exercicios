4f8239d3b588db17841912585b74aa865524a867
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-03-28 15:42:41 -0300

    Finalizando exercicio de fixação

---
+++ front-end/secao-02-Introducao-ao-react/dia-01-hello-world-react/exercise-my-app/src/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+class About extends React.Component {
+    render(){
+        return (
+            <div>
+            <h1>Gabriel Pereira</h1>
+            <p>Olá, eu me chamo Gabriel sou aluno da Trybe e estou estudando para se um software engineer.</p>
+            <h2>Minhas Abilidades</h2>
+            <ul>
+                <li>Organização</li>
+                <li>Colaboração</li>
+                <li>Escuta</li>
+            </ul>
+        </div>
+        );
+    }
+
+}
+
+export default About;
