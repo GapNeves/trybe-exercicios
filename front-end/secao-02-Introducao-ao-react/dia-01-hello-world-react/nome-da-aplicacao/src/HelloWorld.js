f7940223519d2ee91e75167be1df1f1be1ffb355
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-03-28 13:55:37 -0300

    Fazendo meu primeiro Hello World no React

---
+++ front-end/secao-02-Introducao-ao-react/dia-01-hello-world-react/nome-da-aplicacao/src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class HelloWorld extends React.Component {
+    render() {
+        return <span className='HelloWorld'>Hello, World!!</span>
+    }
+}
+
+export default HelloWorld
