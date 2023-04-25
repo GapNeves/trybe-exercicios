c9ed8eef180c9c5c153f1af353fbe486dc10a84e
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-04-25 20:15:01 -0300

    Revendo algumas coisas de React

--- front-end/secao-03-Introducao-ao-react/dia-02-Compoentes-React/exercise-image-component/src/App.js
+++ front-end/secao-03-Introducao-ao-react/dia-02-Compoentes-React/exercise-image-component/src/App.js
@@ -1,13 +1,68 @@
 import React from 'react';
 import Image from './Image';
+import './buttonClick.css';
 
-function App() {
-  const URL = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';
-  return (
-    <main>
-      <Image source={ URL } alternativeText="Cute cat staring" />
-    </main>
+const URL = "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg";
+
+class App extends React.Component {
+  constructor() {
+   super()
+   this.handleClick = this.handleClick.bind(this)
+   this.state = {
+    numeroDeCliuqes: 0,
+   }
+  }
+  handleClick () {
+    this.setState((estadoAnterior, _props) =>({
+      numeroDeCliuqes: estadoAnterior.numeroDeCliuqes + 1,
+    }))
+  };
+
+  handleHi () {
+    console.log('Ola');
+  };
+  handleWorld () {
+    console.log('Mundo');
+  };
+  handleDot () {
+    console.log('!!!');
+  };
+  render() {
+    const {
+      numeroDeCliuqes,
+    } = this.state
+    return (
+      <div>
+      <button
+        onClick={this.handleHi}
+      >
+        Ola
+      </button>
+      <button
+        onClick={this.handleWorld}
+      >
+        Mundo
+      </button>
+      <button
+        onClick={this.handleDot}
+      >
+        !!!
+      </button>
+      <main>
+        <Image source={ URL } alternativeText="Cute cat staring" />
+      </main>
+      <div
+        className="buttonClick"      
+      >
+        <button
+          onClick={this.handleClick}
+          >
+          { numeroDeCliuqes }
+        </button>
+      </div>
+    </div>
   );
+}
 }
 
 export default App;
--- front-end/secao-03-Introducao-ao-react/dia-02-Compoentes-React/exercise-image-component/src/Image.js
+++ front-end/secao-03-Introducao-ao-react/dia-02-Compoentes-React/exercise-image-component/src/Image.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Image.css';
 
 class Image extends React.Component {
   render() {
     const { source, alternativeText } = this.props;
-    return <img src={ source } alt={ alternativeText } />;
+    return <img className="image" src={ source } alt={ alternativeText } />;
   }
 }
 
