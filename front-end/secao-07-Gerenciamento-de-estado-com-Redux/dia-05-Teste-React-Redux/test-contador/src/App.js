36834f87586c02d3e63553f5bcf124baf04030bd
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-05-11 16:05:31 -0300

    Primeiro teste com Redux

--- front-end/secao-07-Gerenciamento-de-estado-com-Redux/dia-05-Teste-React-Redux/test-contador/src/App.js
+++ front-end/secao-07-Gerenciamento-de-estado-com-Redux/dia-05-Teste-React-Redux/test-contador/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
+import { actionCreator } from './redux/actions';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const { dispatch, countState } = this.props;
+    return (
+      <div className="container">
+        <h1>Contador</h1>
+        <h2>{countState}</h2>
+        <button onClick={() => dispatch(actionCreator())}>Incrementa 1</button>
+        <button onClick={() => dispatch(actionCreator(5))}>Incrementa 5</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  countState: state.counterReducer.count,
+});
+
+export default connect(mapStateToProps)(App);
+
--- front-end/secao-07-Gerenciamento-de-estado-com-Redux/dia-05-Teste-React-Redux/test-contador/src/App.test.js
+++ front-end/secao-07-Gerenciamento-de-estado-com-Redux/dia-05-Teste-React-Redux/test-contador/src/App.test.js
@@ -1,8 +1,26 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { combineReducers, legacy_createStore as createStore } from 'redux';
 import { render, screen } from '@testing-library/react';
+
+import counterReducer from './redux/reducers/counterReducer';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const renderWithRedux = (
+  component,
+  {
+    initialState,
+    store = createStore(combineReducers({counterReducer}), initialState)
+  }= {}) => ({
+  ...render(<Provider store={store}>{component}</Provider>),
+  store
+})
+
+test('A página renderiza dois botões e o número 0', () => {
+  renderWithRedux(<App />);
+
+  const buttons = screen.getAllByRole('button');
+
+  expect(buttons).toHaveLength(2);
+  expect(screen.getByText("0")).toBeInTheDocument();
+})
