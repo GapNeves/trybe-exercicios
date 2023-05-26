243be61a366d5b438a53c6db8fe4e8330ca9243a
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-05-26 13:26:38 -0300

    Fim da sessão 7

--- front-end/secao-07-Gerenciamento-de-estado-com-Redux/dia-05-Teste-React-Redux/test-contador/tests/App.test.js
+++ front-end/secao-07-Gerenciamento-de-estado-com-Redux/dia-05-Teste-React-Redux/test-contador/tests/App.test.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 
 import renderWithRedux from './helpers/renderWithRedux';
 import App from '../src/App';
 
-test('A página renderiza dois botões e o número 0', () => {
-  renderWithRedux(<App />);
-
-  const buttons = screen.getAllByRole('button');
-
-  expect(buttons).toHaveLength(2);
-  expect(screen.getByText("0")).toBeInTheDocument();
-})
+describe('Testes do contador:',() => {
+  test('A página renderiza dois botões e o número 0', () => {
+    renderWithRedux(<App />);
+  
+    const buttons = screen.getAllByRole('button');
+  
+    expect(buttons).toHaveLength(2);
+    expect(screen.getByText("0")).toBeInTheDocument();
+  })
+  
+  // test('Incrementa o valor da store ao clicar no botão', () => {
+  //   const { store } = renderWithRedux(<App />);
+  //   expect(screen.getByText('0')).toBeInTheDocument();
+  //   const button = screen.getByText('Incrementa 1');
+  //   userEvent.click(button);
+  //   expect(screen.getByText('1')).toBeInTheDocument();
+  // });
+});
