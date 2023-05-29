0771a79d6b1f40b0bc607bdd604c05197dfd1a34
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-05-29 20:37:40 -0300

    Extraindo o Provider para um componente - atualização

--- front-end/secao-08-Context-API-e-ReactHooks/dia-02-context-API/app-ligthdarkmode/src/App.js
+++ front-end/secao-08-Context-API-e-ReactHooks/dia-02-context-API/app-ligthdarkmode/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ThemeProvider from './context/ThemeProvider';
 
 import './App.css';
@@ -7,19 +7,11 @@ import Header from './components/Header';
 import Image from './components/Image';
 
 function App() {
-  const [themeColor, setThemeColor] = useState('dark');
-
-  function toggleTheme() {
-    setThemeColor(themeColor === 'dark' ? 'ligth' : 'dark');
-  }
-
   return (
     <ThemeProvider>
-      <div className={ themeColor }>
         <Header />
         <Image />
         <Footer />
-      </div>
     </ThemeProvider>
   );
 }
