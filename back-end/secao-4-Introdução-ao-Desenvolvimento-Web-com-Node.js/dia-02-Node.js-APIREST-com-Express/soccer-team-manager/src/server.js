172c3ac29c45621f94d1b33fdb40949e2fdf6a7e
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-07-17 15:27:40 -0300

    Praticando API e revendo conteudo

--- back-end/secao-4-Introdução-ao-Desenvolvimento-Web-com-Node.js/dia-02-Node.js-APIREST-com-Express/soccer-team-manager/src/server.js
+++ back-end/secao-4-Introdução-ao-Desenvolvimento-Web-com-Node.js/dia-02-Node.js-APIREST-com-Express/soccer-team-manager/src/server.js
@@ -1,2 +1,24 @@
-const app = require('./app');
-app.listen(3001, () => console.log('server running on port 3001'));
+const express = require('express');
+
+const app = express();
+
+// app.use(express.json());
+
+// app.get('/', (req, res) => res.status(200).json({message: 'Hello World!'}));
+
+app.get('/teams', (req, res) => {
+  const teams = [
+    {
+      id: 1,
+      name: 'Cruzeiro Esporte Clube',
+      initials: 'CEC',
+    },
+  ];
+
+  res.send(teams);
+  // res.status(201).json({ teams: newTeam })
+});
+
+app.listen(3001, () => {
+  console.log('Start on port 3001');
+});
