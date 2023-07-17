8ce86b6fd0996c6cbc81749fd225e8fc622f08b8
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-07-17 17:41:10 -0300

    Adicionando dados a API com id aleatorio

--- back-end/secao-4-Introdução-ao-Desenvolvimento-Web-com-Node.js/dia-02-Node.js-APIREST-com-Express/soccer-team-manager/src/server.js
+++ back-end/secao-4-Introdução-ao-Desenvolvimento-Web-com-Node.js/dia-02-Node.js-APIREST-com-Express/soccer-team-manager/src/server.js
@@ -1,22 +1,25 @@
+const crypto = require('node:crypto');
 const express = require('express');
+const teams = require('./data');
 
 const app = express();
-
-// app.use(express.json());
+app.use(express.json());
 
 // app.get('/', (req, res) => res.status(200).json({message: 'Hello World!'}));
 
 app.get('/teams', (req, res) => {
-  const teams = [
-    {
-      id: 1,
-      name: 'Cruzeiro Esporte Clube',
-      initials: 'CEC',
-    },
-  ];
+  res.status(200).json(teams);
+});
 
-  res.send(teams);
-  // res.status(201).json({ teams: newTeam })
+app.post('/teams', (req, res) => {
+  const { name, initials } = req.body;
+  const team = {
+    id: crypto.randomUUID(),
+    name,
+    initials,
+  };
+  teams.push(team);
+  res.status(201).json(teams);
 });
 
 app.listen(3001, () => {
