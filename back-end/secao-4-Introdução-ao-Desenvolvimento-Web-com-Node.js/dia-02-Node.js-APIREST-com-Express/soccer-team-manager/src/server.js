da9fa0c3869307b844d1f007a475754994226f3a
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-07-17 19:18:38 -0300

    Finalizando meu primeiro CRUD

--- back-end/secao-4-Introdução-ao-Desenvolvimento-Web-com-Node.js/dia-02-Node.js-APIREST-com-Express/soccer-team-manager/src/server.js
+++ back-end/secao-4-Introdução-ao-Desenvolvimento-Web-com-Node.js/dia-02-Node.js-APIREST-com-Express/soccer-team-manager/src/server.js
@@ -1,6 +1,6 @@
 const crypto = require('node:crypto');
 const express = require('express');
-const teams = require('./data');
+let teams = require('./data');
 
 const app = express();
 app.use(express.json());
@@ -11,8 +11,24 @@ app.get('/teams', (req, res) => {
   res.status(200).json(teams);
 });
 
+// Criando um novo time atraves do .post;
 app.post('/teams', (req, res) => {
+  // Desestruturando chaves para criar um time no Thunder Cliente;
   const { name, initials } = req.body;
+
+  //Verificações para que possa criar corretamente um time;
+  if(!name) return res.status(400).json({ message: 'The name is missing :/' })
+  if(typeof name !== 'string'){
+    return res
+      .status(400).json({ message: 'The name ought to be a string' })
+  }
+  if(!initials) return res.status(400).json({ message: 'The initials is missing :/' })
+  if(typeof initials !== 'string'){
+    return res
+      .status(400).json({ message: 'The initials ought to be a string' })
+  }
+
+  //Gerando um id aleatorio;
   const team = {
     id: crypto.randomUUID(),
     name,
@@ -20,6 +36,72 @@ app.post('/teams', (req, res) => {
   };
   teams.push(team);
   res.status(201).json(teams);
+});
+
+// Deletando um time;
+app.delete('/teams/:id', (req, res) => {
+  // Verificação para que ao tentar deletar um time que já foi deletado retorne 404;
+  const { id } = req.params;
+  const found = teams.find((team) => team.id === id);
+  if(!found) return res.status(404).end();
+
+  teams = teams.filter((team) => team.id !== id);
+  res.status(204).end();
+});
+
+// ATULIZAÇÃO COMPLETA: PUT;
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const{ name, initials } = req.body;
+  const found = teams.find((team) => team.id === id)
+  if (!found) return res.status(404).end();
+
+  //Verificações para que possa criar corretamente um time;
+  if(typeof name !== 'string'){
+    return res
+      .status(400).json({ message: 'The name ought to be a string' })
+  }
+  if(!initials) return res.status(400).json({ message: 'The initials is missing :/' })
+  if(typeof initials !== 'string'){
+    return res
+      .status(400).json({ message: 'The initials ought to be a string' })
+  }
+
+  teams = teams.map(team => {
+    if(team.id === id) {
+      return {
+        // ...team vai buscar todos as chaves;
+        ...team,
+        name,
+        initials,
+      };
+    }
+    return team;
+  })
+  res.status(204).end();
+});
+
+// ATUALIZAÇÃO PARCIAL: PATCH;
+app.patch('/teams/:id/name', (req, res) => {
+  const { id } = req.params;
+  const{ name } = req.body;
+  if (!name) return res.status(404).json({ message: 'The name is missing'});
+  if(typeof name !== 'string'){
+    return res
+      .status(400).json({ message: 'The name ought to be a string' })
+  }
+  const found = teams.find((team) => team.id === id)
+  if (!found) return res.status(404).end();
+  teams = teams.map((team) => {
+    if (team.id === id) {
+      return {
+        ...team,
+        name,
+      };
+    }
+    return team;
+  });
+  res.status(204).end();
 });
 
 app.listen(3001, () => {
