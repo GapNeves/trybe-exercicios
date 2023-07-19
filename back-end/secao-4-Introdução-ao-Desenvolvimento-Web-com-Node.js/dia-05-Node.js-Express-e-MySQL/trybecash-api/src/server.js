f21ec9e0f0c548cef390f6732771ebdf885e82b4
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-07-19 14:45:10 -0300

    Fazendo conexão com MySQL

---
+++ back-end/secao-4-Introdução-ao-Desenvolvimento-Web-com-Node.js/dia-05-Node.js-Express-e-MySQL/trybecash-api/src/server.js
@@ -0,0 +1,9 @@
+// src/server.js
+const app = require('./app');
+const connection = require('./db/connection');
+
+const PORT = 3002;
+
+app.listen(PORT, async () => {
+  console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+});
