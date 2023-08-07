057b26e47a583f2af3832fefa0a82c799d754b50
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-08-07 15:19:36 -0300

    :chore: starting a new section

--- back-end/secao-4-Introdução-ao-Desenvolvimento-Web-com-Node.js/dia-05-Node.js-Express-e-MySQL/trybecash-api/src/db/connection.js
+++ back-end/secao-4-Introdução-ao-Desenvolvimento-Web-com-Node.js/dia-05-Node.js-Express-e-MySQL/trybecash-api/src/db/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 33060,
+  port: process.env.MYSQL_PORT || 3306,
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'root',
   database: process.env.DB_NAME || 'trybecashdb',
