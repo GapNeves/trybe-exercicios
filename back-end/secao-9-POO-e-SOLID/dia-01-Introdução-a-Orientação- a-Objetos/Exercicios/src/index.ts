c2ccf29b9e3a2d54ce9cd4a377ee499b859cb8a1
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-09-19 12:00:07 -0300

    Finalizando primeira parte do exercicio

--- back-end/secao-9-POO-e-SOLID/dia-01-Introdução-a-Orientação- a-Objetos/Exercicios/src/index.ts
+++ back-end/secao-9-POO-e-SOLID/dia-01-Introdução-a-Orientação- a-Objetos/Exercicios/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello guys!!');
+import Estudante from './escola';
+
+const estudante1 = new Estudante('1567', 'Gabriel', [7, 8, 9, 5], [9, 6]);
+console.log(estudante1);
+console.log('Soma das notas: ', estudante1.sumNotas());
+console.log('Média das notas: ', estudante1.medNotas());
+
+// const escola1 = new Escola();
+// console.log(escola1);
